fc5f3f0e139eff110c5cd6e87a959e35a6e16e71
BrahimHbaieb <135034831+BrahimHbaieb@users.noreply.github.com>
2023-07-07 15:41:29 -0400

    class biblio

--- Scripts/class_Bibliotheque.py
+++ Scripts/class_Bibliotheque.py
@@ -1,3 +1,5 @@
 class bibliotheque
     def __init__(self, num ):
         self.num = num
+Parie 1
+On vous à confier la tâche de produire un logiciel de gestion de petite bibliothèque. Vous êtes responsable de la partie logique et vous devez produire une première version sur console pour tester le tout.
