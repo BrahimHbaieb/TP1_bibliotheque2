ef9c2212f0d0ea4f5a2e9918b3f7b8f3e2e05fbb
BrahimHbaieb <135034831+BrahimHbaieb@users.noreply.github.com>
2023-07-07 15:42:35 -0400

    class biblio

--- Scripts/class_Bibliotheque.py
+++ Scripts/class_Bibliotheque.py
@@ -3,3 +3,5 @@ class bibliotheque
         self.num = num
 Parie 1
 On vous à confier la tâche de produire un logiciel de gestion de petite bibliothèque. Vous êtes responsable de la partie logique et vous devez produire une première version sur console pour tester le tout.
+Parie 1
+On vous à confier la tâche de produire un logiciel de gestion de petite bibliothèque. Vous êtes responsable de la partie logique et vous devez produire une première version sur console pour tester le tout.
